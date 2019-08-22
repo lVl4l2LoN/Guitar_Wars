ade19673c9a90a712e5d064c88048188267a75a3
Marlon <m.c4n1as@gmail.com>
2019-08-22 15:01:44 -0700

    updated add score and added new variable constant
    added a max file size constant variable in the appVars.php file. Improved the addScore.php with statements that validate the image file. The application displays error message if the image file is exceeds 32 KB in size and if the file is not the right type (i.e. png, gif, jpeg).

--- addScore.php
+++ addScore.php
@@ -14,36 +14,51 @@
                 $name = $_POST['name'];
                 $score = $_POST['score'];
                 $screenshot = $_FILES['screenshot']['name'];
+                $screenshot_type = $_FILES['screenshot']['type'];
+                $screenshot_size = $_FILES['screenshot']['size'];
 
                 if(!empty($name)&&!empty($score)&&!empty($screenshot)){
+                    if((($screenshot_type == 'image/gif')|| ($screenshot_type == 'image/jpeg') ||
+                    ($screenshot_type == 'image/pjpeg') || ($screenshot_type == 'image/png')) &&
+                    ($screenshot_size > 0) && ($screenshot_size <= GW_MAXFILESIZE)){
+                        if($_FILES['file']['error']==0){
+                            //move the file to the target upload folder
+                            $target = GW_UPLOADPATH . $screenshot;
+                            if(move_uploaded_file($_FILES['screenshot']['tmp_name'],$target)){
 
-                    //move the file to the target upload folder
-                    $target = GW_UPLOADPATH . $screenshot;
-                    if(move_uploaded_file($_FILES['screenshot']['tmp_name'],$target)){
+                                    //connect to the database
+                                $dbc = mysqli_connect( DB_HOST ,DB_USER,DB_PASSWORD,DB_NAME);
 
-                            //connect to the database
-                        $dbc = mysqli_connect( DB_HOST ,DB_USER,DB_PASSWORD,DB_NAME);
+                                //Write to the database
+                                $query = "INSERT INTO guitarwars VALUES(0,NOW(),'$name','$score','$screenshot')";
+                                mysqli_query($dbc,$query);
 
-                        //Write to the database
-                        $query = "INSERT INTO guitarwars VALUES(0,NOW(),'$name','$score','$screenshot')";
-                        mysqli_query($dbc,$query);
+                                //Confirm success
+                                echo '<p>Thanks for adding your new high score!</p>';
+                                echo '<p><strong>Name: </strong>' . $name . '<br>';
+                                echo '<strong>Score: </strong>' . $score . '<br>';
+                                echo '<img src="'.GW_UPLOADPATH. $screenshot.'" alt="Score image"></p>';
+                                echo '<p><a href="index.php">&lt;&lt; Back to high scores</a></p>';
 
-                        //Confirm success
-                        echo '<p>Thanks for adding your new high score!</p>';
-                        echo '<p><strong>Name: </strong>' . $name . '<br>';
-                        echo '<strong>Score: </strong>' . $score . '<br>';
-                        echo '<img src="'.GW_UPLOADPATH. $screenshot.'" alt="Score image"></p>';
-                        echo '<p><a href="index.php">&lt;&lt; Back to high scores</a></p>';
+                                //Clear the score data to clear the form
+                                $name = "";
+                                $score = "";
 
-                        //Clear the score data to clear the form
-                        $name = "";
-                        $score = "";
-
-                        mysqli_close($dbc);
+                                mysqli_close($dbc);
+                            }
+                            else{
+                                echo '<p class="error">There was an error uploading your image.</p>';
+                            }
+                        }
                     }
+
                     else{
-                        echo '<p class="error">There was an error uploading your image.</p>';
+                        echo '<p class = "error">The screenshot must be a GIF, JPEG, or PNG image file'. 
+                        ' no greater than '. (GW_MAXFILESIZE/1024) .' KB in size.</p>';
                     }
+
+                    //Try to delete the temporary screen shot image file
+                    @unlink($_FILES['screenshot']['tmp_name']);
                 }
 
                 else{
