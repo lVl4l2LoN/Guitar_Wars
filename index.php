d88d621e9088d36d93ea6193fb1d3e60cc8f23be
Marlon <m.c4n1as@gmail.com>
2019-08-22 02:34:55 -0700

    index.php
    Changed the main page (ie index.php) to allow the images that were saved in the database to be displayed.

--- index.php
+++ index.php
@@ -15,19 +15,28 @@
 
         <?php
             //connect to the database
-            $dbc = mysqli_connect('local','root','','gwdb');
+            $dbc = mysqli_connect('local','root','','gwdb')
+            or die('Error connecting to MYSQL server');
 
         //get the score data from mysqli
-            $query = "SELECT * FROM guitarWars";
-            $data = mysqli_query($dbc,$query);
+            $query = "SELECT * FROM guitarwars";
+            $data = mysqli_query($dbc,$query)
+            or die('Error querying database');
 
+//Loop through the array of score data and format it as HTML
             echo '<table>';
             while($row=mysqli_fetch_array($data)){
                 //Display the score data
                 echo '<tr> <td class="scoreInfo">';
                 echo '<span class="score">'. $row['score']. '</span><br>';
                 echo '<strong>Name</strong> ' . $row['name'] . '<br>';
-                echo '<strong>Date:</strong> ' . $row['date'] . '</td></tr>';
+                echo '<strong>Date:</strong> ' . $row['date'] . '</td>';
+                if(is_file($row['screenshot']) && filesize($row['screenshot'])>0){
+                    echo '<td><img src="'. $row['screenshot'] . '"alt="Score image"></td></tr>';
+                }
+                else{
+                    echo '<td><img src="unverified.gif" alt="Unverified score"></td></tr>';
+                }
             }
             echo '</table>';
 
