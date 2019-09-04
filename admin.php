69f56edbfa000301fd509a11db08dfb9c34768d7
lVl4l2LoN <m.c4n1as@gmail.com>
2019-09-04 12:45:19 -0700

    http authentication
    updated the admin.php to have http authentication to allow only a select few to have access to the admin page

--- admin.php
+++ admin.php
@@ -1,4 +1,18 @@
 <!doctype html>
+<?php
+        //user name and password 
+        $userName = 'rock';
+        $password = 'roll';
+
+        if(!isset($_SERVER['PHP_AUTH_USER'])||!isset($_SERVER['PHP_AUTH_PW'])||
+        ($_SERVER['PHP_AUTH_USER'] !=$userName)||($_SERVER['PHP_AUTH_PW'] !=$password)){
+            // The user name and or password are incorrect 
+            header('HTTP/1.1 401 Unauthorized');
+            header('WWW-Authenticate: Basic realm = "Guitar Wars"');
+            exit('<h2>Guitar Wars</h2> Sorry, you must enter a valid user name and password to' . 
+            'access this page.');
+        }
+?>
 <html>
     <head>
         <title>Admin Pae</title>
@@ -8,6 +22,7 @@
         <h2>Guitar Wars - High Scores Administration</h2>
         <p>Below is a list of all Guitar Wars high scores. Use this page to remove scores as needed.</p>
         <hr>
+
         <?php
             require_once('appVars.php');
             require_once('connectVars.php');
